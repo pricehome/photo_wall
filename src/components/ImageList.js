6ca5fa18c923a1eda3f684fff836ad5c8a03651e
Mark Price <pricehome@gmail.com>
2019-08-04 15:07:39 -0500

    sunday

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const ImageList = props => {
 	const imgs = props.foundImages.map(img => {
-		return <img key={img.id} src={img.urls.regular} alt={img.alt_description} />
+		return <img key={img.id} src={img.urls.small} alt={img.alt_description} />
 	})
 
 	return <div>{imgs}</div>
